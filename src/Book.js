03a8eaf93141fa147c0d4409fe10e26e7492ffd4
Will Olson <will.olson11@gmail.com>
2017-07-13 22:21:15 -0700

    refactor: Convert Book into a stateless functional component

--- src/Book.js
+++ src/Book.js
@@ -1,45 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-class Book extends React.Component {
-  static propTypes = {
-    book: PropTypes.shape({
-      authors: PropTypes.array,
-      imageLinks: PropTypes.shape({
-        thumbnail: PropTypes.string.isRequired
-      }).isRequired,
-      title: PropTypes.string.isRequired
-    }).isRequired,
-    handleChange: PropTypes.func.isRequired
+const Book = (props) => {
+  const handleChange = (event) => {
+    props.handleChange(props.book, event.target.value)
   }
 
-  handleChange = (event) => {
-    this.props.handleChange(this.props.book, event.target.value)
-  }
+  const { book } = props
 
-  render() {
-    const { book } = this.props
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
-          <div className="book-shelf-changer">
-            <select value={book.shelf || 'none'} onChange={this.handleChange}>
-              <option value="none" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+        <div className="book-shelf-changer">
+          <select value={book.shelf || 'none'} onChange={handleChange}>
+            <option value="none" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-        <div className="book-title">{book.title}</div>
-        {book.authors &&
-          <div className="book-authors">{book.authors.join(', ')}</div>
-        }
       </div>
-    )
-  }
+      <div className="book-title">{book.title}</div>
+      {book.authors &&
+        <div className="book-authors">{book.authors.join(', ')}</div>
+      }
+    </div>
+  )
+}
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    authors: PropTypes.array,
+    imageLinks: PropTypes.shape({
+      thumbnail: PropTypes.string.isRequired
+    }).isRequired,
+    title: PropTypes.string.isRequired
+  }).isRequired,
+  handleChange: PropTypes.func.isRequired
 }
 
 export default Book
