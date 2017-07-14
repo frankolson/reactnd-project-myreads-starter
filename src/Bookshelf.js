3f134347b9a27d5be63fc7987494a13e96fecf75
Frank Olson <will.olson11@gmail.com>
2017-07-14 12:55:34 -0700

    refactor: Deconstuct Bookshelf props further

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types'
 
 import Book from './Book'
 
-const Bookshelf = (props) => (
+const Bookshelf = ({books, getBookshelf, shelfChange, title}) => (
   <div className="bookshelf">
-    <h2 className="bookshelf-title">{props.title}</h2>
+    <h2 className="bookshelf-title">{title}</h2>
     <div className="bookshelf-books">
       <ol className="books-grid">
-        {props.books.map((book) => (
+        {books.map((book) => (
           <li key={book.id}>
             <Book
               book={book}
-              getBookshelf={props.getBookshelf}
-              handleChange={props.shelfChange}
+              getBookshelf={getBookshelf}
+              handleChange={shelfChange}
             />
           </li>
         ))}
