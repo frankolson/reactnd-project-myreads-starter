4348d2420732c3c82ee79ae9d7a8d80916046b5f
Will Olson <will.olson11@gmail.com>
2017-07-13 22:16:07 -0700

    refactor: Convert Main into a stateless functional component

--- src/Main.js
+++ src/Main.js
@@ -4,50 +4,57 @@ import { Link } from 'react-router-dom'
 
 import Bookshelf from './Bookshelf'
 
-class Main extends React.Component {
-  static propTypes = {
-    books: PropTypes.arrayOf(PropTypes.shape({
-      shelf: PropTypes.string.isRequired
-    })),
-    shelfChange: PropTypes.func.isRequired
-  }
+const Main = (props) => {
+  const currentlyReading = (
+    props.books.filter((b) => b.shelf === 'currentlyReading')
+  )
 
-  render() {
-    const currentlyReading = this.props.books.filter((b) => b.shelf === 'currentlyReading')
-    const read = this.props.books.filter((b) => b.shelf === 'read')
-    const wantToRead = this.props.books.filter((b) => b.shelf === 'wantToRead')
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <Bookshelf
-              title="Currently Reading"
-              books={currentlyReading}
-              shelfChange={this.props.shelfChange}
-            />
-            <Bookshelf
-              title="Read"
-              books={read}
-              shelfChange={this.props.shelfChange}
-            />
-            <Bookshelf
-              title="Want to Read"
-              books={wantToRead}
-              shelfChange={this.props.shelfChange}
-            />
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search">
-            Add a book
-          </Link>
+  const read = (
+    props.books.filter((b) => b.shelf === 'read')
+  )
+
+  const wantToRead = (
+    props.books.filter((b) => b.shelf === 'wantToRead')
+  )
+
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <Bookshelf
+            title="Currently Reading"
+            books={currentlyReading}
+            shelfChange={props.shelfChange}
+          />
+          <Bookshelf
+            title="Read"
+            books={read}
+            shelfChange={props.shelfChange}
+          />
+          <Bookshelf
+            title="Want to Read"
+            books={wantToRead}
+            shelfChange={props.shelfChange}
+          />
         </div>
       </div>
-    )
-  }
+      <div className="open-search">
+        <Link to="/search">
+          Add a book
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+Main.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.shape({
+    shelf: PropTypes.string.isRequired
+  })),
+  shelfChange: PropTypes.func.isRequired
 }
 
 export default Main
