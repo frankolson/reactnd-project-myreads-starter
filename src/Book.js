f45f8afd29519da21362bba1611b7cd761a07c53
Frank Olson <will.olson11@gmail.com>
2017-07-14 10:16:14 -0700

    fix: Persist shelf change from Search on Search

--- src/Book.js
+++ src/Book.js
@@ -1,33 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Book = (props) => {
-  const handleChange = (event) => {
-    props.handleChange(props.book, event.target.value)
+class Book extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      shelf: props.book.shelf || 'none'
+    }
   }
 
-  const { book } = props
+  handleChange = (event) => {
+    this.setState({shelf: event.target.value})
+    this.props.handleChange(this.props.book, event.target.value)
+  }
 
-  return (
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
-        <div className="book-shelf-changer">
-          <select value={book.shelf || 'none'} onChange={handleChange}>
-            <option value="none" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
+  render() {
+    const { book } = this.props
+    
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+          <div className="book-shelf-changer">
+            <select value={this.state.shelf} onChange={this.handleChange}>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
         </div>
+        <div className="book-title">{book.title}</div>
+        {book.authors &&
+          <div className="book-authors">{book.authors.join(', ')}</div>
+        }
       </div>
-      <div className="book-title">{book.title}</div>
-      {book.authors &&
-        <div className="book-authors">{book.authors.join(', ')}</div>
-      }
-    </div>
-  )
+    )
+  }
 }
 
 Book.propTypes = {
